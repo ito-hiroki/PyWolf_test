fbcd5701aefd0099ec6504256e9c982dfc6441e2
ito-hiroki <i.hiroki0307@icloud.com>
2018-06-19 11:22:30 +0900

    sub_funcを綺麗に

--- sub_func.py
+++ sub_func.py
@@ -1,15 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 10 17:27:00 2018
-
-@author: kumac
-"""
-
 def name2num(agent_name):
     return int(agent_name[6:8])
 
 def num2name(agent_num):
     return "Agent[" + "{0:02d}".format(agent_num) + "]"
 
-print(num2name(5))
     
